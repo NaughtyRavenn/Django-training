40c2712c047ec90039a8a8e089316f73e75f43bd
NaughtyRavenn <bachminh.uit@gmail.com>
2023-12-25 11:23:05 +0700

    Create and modify admin size

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import post
 
-# Register your models here.
+
+class postAdmin(admin.ModelAdmin):
+    ordering = ["id"]
+    list_display = ["id", "title", "date"]
+    list_filter = ["date"]
+    search_fields = ["title"]
+
+
+admin.site.register(post, postAdmin)
