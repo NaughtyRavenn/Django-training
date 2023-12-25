54f2dc4d342c9f4f699d4b38293b9c928df761b9
NaughtyRavenn <bachminh.uit@gmail.com>
2023-12-25 12:05:54 +0700

    Display blog in detail

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import post
 
-# Create your tests here.
+
+class blogTests(TestCase):
+    def setUp(self):
+        post.objects.create(
+            title="My title",
+            body="Just a test",
+        )
+
+    def test_string_represantation(self):
+        Post = post(title="My entry title")
+        self.assertEqual(str(Post), Post.title)
+
+    def test_post_list_view(self):
+        response = self.client.get("/blog/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "My title")
+        self.assertTemplateUsed(response, "blog/blog.html")
+
+    def test_post_detail_view(self):
+        response = self.client.get("/blog/1/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "My title")
+        self.assertTemplateUsed(response, "blog/post.html")
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,8 @@ from .models import post
 def list(request):
     data = {"posts": post.objects.all().order_by("-date")}
     return render(request, "blog/blog.html", data)
+
+
+def postContent(request, id):
+    postContent = post.objects.get(id=id)
+    return render(request, "blog/post.html", {"post": postContent})
